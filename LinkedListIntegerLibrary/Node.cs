249adfacfb2379414cac974e7a33a0154fafd4dc
jered <me@jered.dev>
2022-08-31 11:40:10 -0600

    Initial create and commit.
    Initial repository creation. Created a Node class for a linked list node. This class contains a node value, the previous node, and the next node.

---
+++ LinkedListIntegerLibrary/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListIntegerLibrary
+{
+    public class Node
+    {
+        public int Value;
+        public Node Prev;
+        public Node Next;
+
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+    }
+}
